be45b7d9b2caa0ed92c39e6df50635d544077446
luisbarufi <luisbarufi@gmail.com>
2020-07-28 09:10:56 -0300

    fixing logic

--- main.js
+++ main.js
@@ -1,30 +1,24 @@
-var numbers = document.getElementsByClassName('numbers');
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].onclick = typedNumber;
-}
-
-var operators = document.getElementsByClassName('operators');
-for (let i = 0; i < operators.length; i++) {
-  operators[i].onclick = typedOperator;  
-}
-
 var chosenOperator = '';
 var newValue = '';
 var oldValue = '';
 
 function typedNumber(event) {
   var typedValue = event.target.attributes.value.nodeValue;
+
   updatescreen(`${newValue}${typedValue}`);
 }
 
 function typedOperator(event) {
-  var selectedOperator = event.target.attributes.value.nodeValue;  
+  var selectedOperator = event.target.attributes.value.nodeValue;
+
   rotateAction(selectedOperator);
 }
 
 function updatescreen(value){
   var screen = document.getElementById('screen');
+
   newValue = value;
+  
   screen.innerText = newValue;
 }
 
@@ -46,19 +40,10 @@ function rotateAction(operator) {
         result = Number(oldValue) / Number(newValue);
         break;
     }
-    
+  
     updatescreen(result);
-
+  
   }else{
-
-    if (operator === 'toClian') {
-      updatescreen("");
-    }
-
-    if (operator === 'toClianAll') {
-      location.reload;
-    }
-
     oldValue = newValue;
     updatescreen("");
     chosenOperator = operator;
